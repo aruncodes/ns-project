194533407a02df9b15a46b2ff33f9a9d758b1079
Arun <zyberkiddy@yahoo.com>
2014-10-16 13:52:54 +0530

    cleanup

--- extension/detect.js
+++ extension/detect.js
@@ -43,31 +43,31 @@
 	// chrome.webRequest.onCompleted.addListener(function() {console.log(insertedNodes); } );
 	// window.setInterval(function () { 
 	// 	// console.log("printing nodes.");
-		
+	
 	// },1000);
 
-	function walk(node, callback) {
-	  var skip, tmp;
-	  var depth = 0;
-	 
-	  do {
-	    if ( !skip ) {
-	      skip = callback.call(node, depth) === false;
-	    }
-	 
-	    if ( !skip && (tmp = node.firstChild) ) {
-	      depth++;
-	    } else if ( tmp = node.nextSibling ) {
-	      skip = false;
-	    } else {
-	      tmp = node.parentNode;
-	      depth--;
-	      skip = true;
-	    }
-	 
-	    node = tmp;
-	 
-	  } while ( depth > 0 );
-	}
+function walk(node, callback) {
+	var skip, tmp;
+	var depth = 0;
+	
+	do {
+		if ( !skip ) {
+			skip = callback.call(node, depth) === false;
+		}
+		
+		if ( !skip && (tmp = node.firstChild) ) {
+			depth++;
+		} else if ( tmp = node.nextSibling ) {
+			skip = false;
+		} else {
+			tmp = node.parentNode;
+			depth--;
+			skip = true;
+		}
+		
+		node = tmp;
+		
+	} while ( depth > 0 );
+}
 
 })();
